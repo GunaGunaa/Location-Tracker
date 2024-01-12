16c89680a03e12266be82f10c840501237307679
Gunasekaran413003 <gunasekaran@mallow-tech.com>
2024-01-13 00:14:17 +0530

    splashscreen completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,10 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding =true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +41,7 @@ android {
 
 dependencies {
 
+    //noinspection GradleCompatible
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,4 +49,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.skyfishjy.ripplebackground:library:1.0.1")
 }
--- app/src/main/java/com/example/locationtracker/LTSplashActivity.kt
+++ app/src/main/java/com/example/locationtracker/LTSplashActivity.kt
@@ -3,11 +3,21 @@ package com.example.locationtracker
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.locationtracker.databinding.LtActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class LTSplashActivity : AppCompatActivity() {
+    private lateinit var binding: LtActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= LtActivitySplashBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lt_activity_splash)
+        setContentView(binding.root)
+        binding.rippleBackground.startRippleAnimation()
+        Handler().postDelayed({
+
+
+            binding.rippleBackground.stopRippleAnimation()
+        },4000)
     }
 }
